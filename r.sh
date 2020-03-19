f63bc296d6bcf1eaa48a822d28d83ce4a696fd1c
RobxSor <robyrobyroby10.26.00@gmail.com>
2020-03-19 10:34:22 +0800

    NOT YET FINAL
    Test script for accounts.sh

--- r.sh
+++ r.sh
@@ -1,3 +1,6 @@
+#NOTE: THIS IS NOT THE FINAL SCRIPT.
+#This will be substituted to accounts.sh once fishied.
+
 MY_INPUT='../home/bahay.csv'
 declare -a A_NAME
 declare -a A_USERNAME
@@ -8,7 +11,6 @@ done <"$MY_INPUT"
 clear
 for index in "${!A_USERNAME[@]}"; do
 	echo "Creating ${A_USERNAME[$index]}..."
-    #useradd -m -d "/home/${A_USERNAME[$index]}" -s /bin/bash -p "$(echo "platform" | passwd -stdin)" "${A_USERNAME[$index]}"
-    #chmod
-    useradd -m -d "/home/${A_USERNAME[$index]}" -s /bin/bash -p "$(echo "username:newpassword" | chpasswd)" "${A_USERNAME[$index]}"
+    	#useradd -m -d "/home/${A_USERNAME[$index]}" -s /bin/bash -p "$(echo "platform" | passwd -stdin)" "${A_USERNAME[$index]}"
+    	useradd -m -d "/home/${A_USERNAME[$index]}" -s /bin/bash -p "$(echo "username:newpassword" | chpasswd)" "${A_USERNAME[$index]}"
 done
